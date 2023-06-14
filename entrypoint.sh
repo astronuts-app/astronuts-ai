878d9e2238d8f61a2e860e017fc80e177668dcb0
Arun K Patra <arunkpatra@gmail.com>
2023-06-14 21:27:23 +0530

    Updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+echo GitHub repository       :  "${GITHUB_REPOSITORY}"
+echo GitHub repository owner :  "${GITHUB_REPOSITORY_OWNER}"
+echo GitHub ref              : "${GITHUB_REF}"
+echo GitHub head ref         :  "${GITHUB_HEAD_REF}"
+echo GitHub base ref         :  "${GITHUB_BASE_REF}"
+echo GitHub sha              :  "${GITHUB_SHA}"
+echo GitHub ref name         :  "${GITHUB_REF_NAME}"
+echo GitHub ref type         :  "${GITHUB_REF_TYPE}"
+echo GitHub action           :  "${GITHUB_ACTION}"
+echo GitHub action repo      :  "${GITHUB_ACTION_REPOSITORY}"
+echo GitHub action ref       :  "${GITHUB_ACTION_REF}"
+echo GitHub actor            :  "${GITHUB_ACTOR}"
+
+
 APP_OPTS=
 APP_OPTS="$APP_OPTS -Dspring.profiles.active=prod"
 
