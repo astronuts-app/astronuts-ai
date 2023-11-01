c74ac23d157a0d8f35e85fce5fc5e44fd5809dc9
Arun Patra <arun@astronuts.io>
2023-11-02 00:02:55 +0530

    Server override

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 APP_OPTS=
 APP_OPTS="$APP_OPTS -Dspring.profiles.active=prod"
 
-java $APP_OPTS -jar /hubble-scanner.jar -l="${INPUT_SOURCELANGUAGE}" -r="$INPUT_ROOTDIR"
+java $APP_OPTS -jar /hubble-scanner.jar -l="${INPUT_SOURCELANGUAGE}" -r="$INPUT_ROOTDIR" -u="$INPUT_SERVERURL"
